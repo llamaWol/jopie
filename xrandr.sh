fb5364ac794b1888a09a22789f2156e8a05f83d9
Joost <67469412+llamaWol@users.noreply.github.com>
2023-04-10 17:12:22 +0200

    Delete xrandr.sh

--- xrandr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-hdmi_active=$(xrandr | grep ' connected' | grep 'HDMI' | awk '{print $1}')
-
-[[ -z "$hdmi_active" ]] && xrandr --output eDP1 --mode 2560x1600 --rate 60.00 --primary --scale 0.5x0.5 --output HDMI2 --off
-[[ "$hdmi_active" ]] && xrandr --output HDMI2 --mode 1920x1080 --rate 120.00 --primary --output eDP1 --mode 2560x1600 --scale 0.5x0.5 --below HDMI2
